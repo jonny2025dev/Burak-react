3d092bd7bcad14712be97f9a9afe3fd7308953de
jonny2025dev <akhrorbek0704@icloud.com>
2025-08-21 07:46:43 +0900

    feat:develop AppRootState And HomePage type integrations

---
+++ src/lib/data/types/member.ts
@@ -0,0 +1,42 @@
+import { memberStatus, memberType } from "../enums/member.enum";
+
+export interface Member {
+    _id: string;
+    memberType: memberType;
+    memberStatus: memberStatus;
+    memberNick: string;
+    memberPhone: string;
+    memberPassword?: string;
+    memberAdress?: string;
+    memberDesc?: string;
+    memberImage?: string;
+    memberPoints: number;
+    createdAt: Date;
+    updatedAt: Date;
+  }
+  
+  export interface MemberInput {
+    memberType?: memberType;
+    memberStatus?: memberStatus;
+    memberNick: string;
+    memberPhone: string;
+    memberPassword: string;
+    memberAdress?: string;
+    memberDesc?: string;
+    memberImage?: string;
+    memberPoints?: number;
+  }
+  
+  export interface LoginInput {
+    memberNick: string;
+    memberPassword: string;
+  }
+  
+  export interface MemberUpdateInput {
+    memberNick?: string;
+    memberPhone?: string;
+    memberPassword?: string;
+    memberAdress?: string;
+    memberDesc?: string;
+    memberImage?: string;
+  }
