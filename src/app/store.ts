9644ad96957e590a0adc3bc748106885feb85076
jonny2025dev <akhrorbek0704@icloud.com>
2025-08-21 13:24:46 +0900

    feat: middleware integration redux loger

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,12 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import HomePageReducer from "./screens/homePage/slice";
+import ReduxLogger  from "redux-logger";
+
 
 export const store = configureStore({
+  middleware: (getDefaultMiddleware) =>  
+    // @ts-ignore
+    getDefaultMiddleware().concat(ReduxLogger),
   reducer: {
     homePage:  HomePageReducer,
   },
