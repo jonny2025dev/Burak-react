03b7394c980441dfac789da79ed30146c7436323
jonny2025dev <akhrorbek0704@icloud.com>
2025-08-23 17:43:40 +0900

    feat: develop product service business to retreive data from backend server

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -7,27 +7,37 @@ import ActiveUsers from "./ActiveUsers";
 import Events from "./Events";
 import "../../../css/home.css";
 
-import { useDispatch, useSelector } from "react-redux";
-import { Dispatch } from "@reduxjs/toolkit"; 
-import { createSelector } from "reselect"; 
+import { useDispatch } from "react-redux";
+import { Dispatch } from "@reduxjs/toolkit";  
 import { setPopularDishes } from "./slice";
-import { retrievePopularDishes } from "./selector";
 import { Product } from "../../../lib/data/types/product";
+import { ProductCollection } from "../../../lib/data/enums/product.enum";
+import ProductService from "../../services/ProductService";
 
 /** REDUX SLICE & SELECTOR */
 const actionDispatch = (dispatch: Dispatch) => ({
   setPopularDishes: (data: Product[]) => dispatch(setPopularDishes (data)), 
 });
-const popularDishesRetriever = createSelector(
-  retrievePopularDishes,
-  (popularDishes) => ({ popularDishes })
-);
 
 export default function HomePage() {
   const { setPopularDishes } = actionDispatch (useDispatch ());
-  const { popularDishes } = useSelector (popularDishesRetriever);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    // Backend server data fetch => Data
+    const product = new ProductService();
+    product
+      .getProducts({
+        page: 1, 
+        limit: 4, 
+        order: "productViews",
+        productCollection: ProductCollection.DISH,
+      })
+      .then((data) => {
+         console.log("data passed here:", data);
+         setPopularDishes(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
     <div className={"homepage"}>
